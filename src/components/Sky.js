623e00a60f9ec26bef2b7479bc8f29be1b222695
Ben Pickles <spideryoung@gmail.com>
2017-04-15 10:01:04 +0100

    Dark sky at night.

--- src/components/Sky.js
+++ src/components/Sky.js
@@ -2,15 +2,33 @@ import React from 'react'
 import '../css/Sky.css'
 
 const MINS_IN_DAY = 60 * 24
+const MORNING = 60 * 6
+const NIGHT = 60 * 18
 
 export default ({ time }) => {
   const mins = (time.hours() * 60) + time.minutes()
+
+  let opacity
+
+  if (mins < MORNING) {
+    opacity = 1 - (mins / MORNING)
+  } else if (mins > NIGHT) {
+    opacity = mins / MINS_IN_DAY
+  } else {
+    opacity = 0
+  }
+
   const percentThroughDay = mins / MINS_IN_DAY
   const degreesThroughDay = Math.round(percentThroughDay * 360)
   const transform = `rotate(-${degreesThroughDay}deg)`
 
   return (
     <div className="Sky">
+      <div
+        className="Sky-dark"
+        style={{ opacity }}
+      />
+
       <div
         className="Sky-rotating"
         style={{ transform }}
